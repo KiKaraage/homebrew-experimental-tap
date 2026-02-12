733ebac419e27a08a298736c48707f027d07998d
KiKaraage <kikaraage@tuta.io>
2026-02-12 08:41:33 +0700

    fix: adjust indents & fileutils for Craft Agents

--- Casks/craft-agents-linux.rb
+++ Casks/craft-agents-linux.rb
@@ -5,7 +5,7 @@ cask "craft-agents-linux" do
   sha256 x86_64_linux: "e7bc0b62eafe4701aa1924933ddefca03f5ddbf2147d3c2414397079272d21fd"
 
   url "https://github.com/lukilabs/craft-agents-oss/releases/download/v#{version}/Craft-Agents-#{version}-linux-x64.AppImage"
-    verified: "github.com/lukilabs/craft-agents-oss"
+  verified "https://github.com/lukilabs/craft-agents-oss"
   name "Craft Agents"
   desc "Work with most powerful agents in the world, with the UX they deserve"
   homepage "https://agents.craft.do/"
@@ -27,7 +27,7 @@ cask "craft-agents-linux" do
     appimage_path = "#{staged_path}/Craft-Agents-#{version}-linux-x64.AppImage"
     system "chmod", "+x", appimage_path
     system appimage_path, "--appimage-extract", chdir: staged_path
-    FileUtils.rm_f appimage_path
+    FileUtils.rm(appimage_path)
   end
 
   postflight do
@@ -35,14 +35,15 @@ cask "craft-agents-linux" do
     if File.exist?(desktop_target)
       desktop_content = File.read(desktop_target)
       desktop_content.gsub!(/^Exec=AppRun/, "Exec=#{HOMEBREW_PREFIX}/bin/craft-agents")
-      desktop_content.gsub!(/^Icon=.*/, "Icon=#{Dir.home}/.local/share/icons/hicolor/512x512/apps/@craft-agentelectron.png")
+      desktop_content.gsub!(/^Icon=.*/,
+                            "Icon=#{Dir.home}/.local/share/icons/hicolor/512x512/apps/@craft-agentelectron.png")
       File.write(desktop_target, desktop_content)
     end
   end
 
   uninstall_postflight do
-    FileUtils.rm_f("#{Dir.home}/.local/share/icons/hicolor/512x512/apps/@craft-agentelectron.png")
-    FileUtils.rm_f("#{Dir.home}/.local/share/applications/@craft-agentelectron.desktop")
+    FileUtils.rm("#{Dir.home}/.local/share/icons/hicolor/512x512/apps/@craft-agentelectron.png")
+    FileUtils.rm("#{Dir.home}/.local/share/applications/@craft-agentelectron.desktop")
   end
 
   zap trash: [
@@ -50,4 +51,3 @@ cask "craft-agents-linux" do
     "~/.craft-agent",
   ]
 end
-
